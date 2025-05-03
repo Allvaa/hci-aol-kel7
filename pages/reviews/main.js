8a82b267db84994b274fd5cbadf41b11f8912af1
Allvaa <alvamailindo@gmail.com>
2025-05-03 23:53:46 +0700

    review pop up

--- pages/reviews/main.js
+++ pages/reviews/main.js
@@ -1,3 +1,21 @@
 document.querySelector(".backbutton").addEventListener("mouseup", () => {
     location.pathname = "pages/details"
 });
+
+function toggleAddReview() {
+    const dim = document.querySelector(".dim");
+    const addreview = document.querySelector(".addreview");
+
+    dim.classList.contains("hidden") ? dim.classList.remove("hidden") : dim.classList.add("hidden");
+    addreview.classList.contains("hidden") ? addreview.classList.remove("hidden") : addreview.classList.add("hidden");
+}
+
+document.querySelector("a").addEventListener("mouseup", () => {
+    toggleAddReview();
+});
+
+document.addEventListener("mouseup", e => {
+    if (e.target.className === "dim") {
+        toggleAddReview();
+    }
+});
