7f8053dcca6018ddf4f78d0ce575d7a4734327f6
allvaa <52526068+Allvaa@users.noreply.github.com>
2025-05-03 18:25:59 +0700

    Update main.js

--- pages/home/main.js
+++ pages/home/main.js
@@ -1,3 +1,3 @@
 document.querySelector(".resto").addEventListener("mouseup", () => {
-    location.pathname = "pages/details/index.html"
+    location.pathname = (location.pathname.startsWith("/hci-aol-kel7") ? "hci-aol-kel7/" : "" ) + "pages/details/index.html"
 });
