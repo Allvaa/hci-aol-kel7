9c42a5d37bd213fb10c95d0d2c686253c6617bf2
allvaa <52526068+Allvaa@users.noreply.github.com>
2025-05-03 18:05:37 +0700

    Update main.js

--- pages/details/main.js
+++ pages/details/main.js
@@ -1,8 +1,8 @@
 document.querySelector(".backbutton").addEventListener("mouseup", () => {
-    location.pathname = "pages/home.html"
+    location.pathname = "pages/home/index.html"
 });
 
 document.querySelector("a").addEventListener("mouseup", (e) => {
     e.preventDefault();
-    location.pathname = "pages/reviews.html"
+    location.pathname = "pages/reviews/index.html"
 });
